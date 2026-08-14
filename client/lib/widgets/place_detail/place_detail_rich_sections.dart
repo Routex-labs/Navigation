893ae1c5f078861e09662030cefce8367b7c5bce
hyunkim <kimhyun4103@gmail.com>
2026-08-14 18:22:55 +0900

    feat: 매장 상세 정보의 위계와 가독성을 개선한다

--- client/lib/widgets/place_detail/place_detail_rich_sections.dart
+++ client/lib/widgets/place_detail/place_detail_rich_sections.dart
@@ -1568,8 +1568,9 @@ class PlaceSectionTitle extends StatelessWidget {
   Widget build(BuildContext context) => Text(
     text,
     style: const TextStyle(
-      fontSize: 17,
-      fontWeight: FontWeight.w800,
+      fontSize: 16,
+      height: 1.3,
+      fontWeight: FontWeight.w700,
       color: AppColors.text,
     ),
   );
